37db03e3d301efba7f4ef13660caffbfc26b9777
Dao Thanh Tung (MS/EDA33-XC) <Tung.DaoThanh@vn.bosch.com>
2024-12-20 14:31:16 +0700

    Qt6QMLForBeginners 04-CustomComponentsAndModules

---
+++ Learning/Udemy/Qt6_QML_For_Beginner_The_Fundamentals/04-CustomComponentsAndModules/v04QMLModuleQMake/main.cpp
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * author dta3hc - thanhtungpfiev@gmail.com
+ *
+ * date 20 Dec 2024
+ *
+ * copyright 2024 - thanhtungpfiev@gmail.com. All rights reserved.
+ *
+ * filename %{Cpp:License:FileName}
+ *
+ * class %{Cpp:License:ClassName}
+ *
+ * brief
+ *
+ *
+ *****************************************************************************/
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.addImportPath("qrc:/");
+    engine.load(url);
+
+    return app.exec();
+}
