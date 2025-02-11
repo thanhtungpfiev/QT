17c65936d9eb8b88194c82c43d80fc52721d78f0
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2025-02-11 21:28:29 +0700

    Qt6QMLForBeginners 12-Transforms, Animations, States and Transitions

---
+++ Learning/Udemy/Qt6_QML_Beginner/12-Transforms/v03AnimationsIntro/main.cpp
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ * author DTA3HC - thanhtungpfiev@gmail.com
+ *
+ * date 2/11/2025
+ *
+ * copyright 2025 - thanhtungpfiev@gmail.com. All rights reserved.
+ *
+ * filename %{Cpp:License:FileName}
+ *
+ * class %{Cpp:License:ClassName}
+ *
+ * brief
+ *
+ *
+ *****************************************************************************/
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("v03AnimationsIntro", "Main");
+
+    return app.exec();
+}
